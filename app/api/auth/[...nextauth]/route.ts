bdd71a36fa86a52604fad1af5f89f8ecfce4459c
Codefreak69 <iamvikashkumar30@gmail.com>
2023-05-31 06:35:16 +0530

    basic project setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,7 @@
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth/next";
+import { NextRequest, NextResponse } from "next/server";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface DashboardProps {}
+
+const Dashboard: FunctionComponent<DashboardProps> = () => {
+  return <div>Hello Dashboard</div>;
+};
+
+export default Dashboard;
